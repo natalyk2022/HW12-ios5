c3c9ac66df46d0f5b5491678cc7ae3e94d5eeef4
Nataly <natalyk2022@gmail.com>
2022-06-20 23:54:12 +0300

    Initial Commit

---
+++ HW12-ios5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12-ios5
+//
+//  Created by  Nataly on 20.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
