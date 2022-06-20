0ecedffd0cebba9e7fa1d604ee49bbcb31c65ec1
Nataly <natalyk2022@gmail.com>
2022-06-21 00:11:45 +0300

    create storyboard and connection

--- HW12-ios5/ViewController.swift
+++ HW12-ios5/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBAction func playPauseAction(_ sender: Any) {
+    }
+    @IBOutlet weak var playPauseAction: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
